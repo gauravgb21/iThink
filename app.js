c80b3d426e1d7ca8883e3696b6f219e082b592f8
gauravgb21 <sharmagaurav7892@gmail.com>
2018-01-21 00:42:05 +0530

    testing app

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express=require('express');
 var path=require('path');
+var cookieParser=require('cookie-parser');
 var bodyParser=require('body-parser');
 var exphbs=require('express-handlebars');
 var expressValidator=require('express-validator');
 var passport=require('passport');
 var LocalStrategy=require('passport-local').Strategy;
 var flash=require('connect-flash');
+var session=require('express-session');
+var mongo=require('mongodb');
+var mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/loginapp');
+var db=mongoose.connection;
 
+
+//Routes
 var routes=require('./routes/index');
 var users=require('./routes/users');
 
@@ -25,3 +33,58 @@ app.set('view engine','handlebars');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+
+//static folders
+app.use(express.static(path.join(__dirname,'public')));
+
+//Express sessions
+app.use(session({
+ secret: 'secret',
+ saveUninitialized: true,
+ resave: true
+}));
+
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Express Validators
+app.use(expressValidator({
+errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//connect flash
+app.use(flash());
+
+//Global Variables
+app.use(function(req,res,next){
+res.locals.success_msg=req.flash('success_msg');
+res.locals.error_msg=req.flash('error_msg');
+res.locals.error=req.flash('error');
+next();
+});
+
+//connect routes middlewares
+app.use('/',routes);
+app.use('/users',users);
+
+//connect app
+
+app.listen(8081,function(){
+	console.log('server is listing to 8081');
+});
